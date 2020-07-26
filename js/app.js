ae3bfdd7a96330bd019ad3c6cbfd25ef1c09930e
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-07-26 16:24:42 +0300

    Alpha_Version

--- js/app.js
+++ js/app.js
@@ -11,62 +11,82 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 
-let navbar = document.querySelector('#navbar__list');
-let allsections = document.querySelectorAll('section');
-
+let navBar = document.querySelector('.topnav');
+let allSections = document.querySelectorAll('section');
+let allHref = document.querySelectorAll('a')
+let anchor = null;
+let sectionCount = document.getElementsByClassName('landing__container').length;
+let sectionList = [];
 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
-let sectionCount = document.getElementsByClassName('landing__container').length;
-let sectionList = [];
-
-
+function isInViewport(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-   
-for (let i = 0; i < sectionCount; i++){
+
+function buildNav() {
+    for (let i = 0; i < sectionCount; i++) {
+        anchor = document.createElement('a');
+        j = i + 1;
+        anchor.href = '#section' + j;
+        anchor.id = 'button' + j;
         sectionList[i] = document.createTextNode(`Section ${i+1}`);
+        anchor.appendChild(sectionList[i]);
+        navBar.appendChild(anchor);
     }
-    let navHeading = document.getElementsByClassName('topnav');
-    let anchor = document.createElement('a');
-    anchor.appendChild(sectionList);
-    navbar.appendChild(anchor);
+}
 
 
-// Add class 'active' to section when near top of viewport
 
 
-// Scroll to anchor ID using scrollTO event
+
 
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
+
+window.addEventListener('scroll', function(event) {
+    for (let i = 0; i < sectionCount; i++) {
+        elem = allSections.item(i);
+        if (isInViewport(elem)) {
+            elem.classList.add('your-active-class');
+        } else {
+            elem.classList.remove('your-active-class')
+        }
+    }
+}, false);
 
 // Build menu 
-
+buildNav();
 // Scroll to section on link click
 
 // Set sections as active
-
-
